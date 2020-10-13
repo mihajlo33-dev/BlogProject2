7b928a368b4e39fca14029a62109d95409997d6c
Mihajlo Drljaca <mdrljaca@rocketmail.com>
2020-10-13 19:33:27 +0200

    Full Project

---
+++ BlogProject/Food/Food-Article2.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="Food-Article2.aspx.cs" Inherits="BlogProject.Food.Food_Article2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3>Food Article 2</h3>
+    <hr />
+
+</asp:Content>
---
+++ BlogProject/Food/www.ham-bur.com.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlogProject.Food
+{
+    public partial class www_ham_bur_com : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
